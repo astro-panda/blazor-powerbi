7b9007012f445c72a18408c1e898760fc567b0d0
Turner Bass <turner@psypher9.com>
2022-05-17 22:17:36 -0500

    beginning work on service collection extensions

---
+++ AstroPanda.Blazor.PowerBI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using AstroPanda.Blazor.PowerBI.Options;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+
+        public static IServiceCollection AddBlazorPowerBI(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddBlazorPowerBI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<PowerBIOptions>(x => configuration.GetSection("PowerBI"));
+            return services;
+        }
+    }
+}
