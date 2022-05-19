72e53e0b0e26f37bdf49ef9dea5095b743e6c4d8
Turner Bass <turner@psypher9.com>
2022-05-19 17:23:12 -0500

    fix the configuration issues; add dependency injection for MS Power BI Client with authentication; blank blazor power bi js

--- AstroPanda.Blazor.PowerBI/Extensions/ServiceCollectionExtensions.cs
+++ AstroPanda.Blazor.PowerBI/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using AstroPanda.Blazor.PowerBI.Options;
+using AstroPanda.Blazor.PowerBI.Services;
 using Microsoft.Extensions.Configuration;
+using Microsoft.PowerBI.Api;
+using Microsoft.Rest;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +21,16 @@ namespace Microsoft.Extensions.DependencyInjection
 
         public static IServiceCollection AddBlazorPowerBI(this IServiceCollection services, IConfiguration configuration)
         {
-            services.Configure<PowerBIOptions>(x => configuration.GetSection("PowerBI"));
+            services.Configure<PowerBIOptions>(x => configuration.Bind(x));
+            services.AddTransient<PowerBIInterop>();
+            services.AddTransient<IPowerBIAuthenticationService, PowerBIAuthenticationService>();
+            services.AddTransient<PowerBIClient>(sp =>
+            {
+                var auth = sp.GetRequiredService<IPowerBIAuthenticationService>();
+                var authResult = auth.GetAuthentication().GetAwaiter().GetResult();
+                var creds = new TokenCredentials(authResult.AccessToken, "Bearer");
+                return new PowerBIClient(creds);
+            });
             return services;
         }
     }
--- AstroPanda.Blazor.PowerBI/Services/PowerBIInterop.cs
+++ AstroPanda.Blazor.PowerBI/Services/PowerBIInterop.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.JSInterop;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Microsoft.PowerBI.Api;
+using Microsoft.PowerBI.Api.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +12,15 @@ namespace AstroPanda.Blazor.PowerBI.Services
 {
     public class PowerBIInterop : IAsyncDisposable
     {
-        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;        
+        private readonly Lazy<Task<IJSObjectReference>> embedModuleTask;        
+        private readonly PowerBIClient _powerBi;
 
-        public PowerBIInterop(IJSRuntime jsRuntime)
+        public PowerBIInterop(IJSRuntime jsRuntime, PowerBIClient powerBI)
         {
-            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
-                "import", "./_content/powerbi-client/dist/powerbi.js").AsTask());
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/powerbi-client/dist/powerbi.js").AsTask());
+            embedModuleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/blazor-power-bi.js").AsTask());
+            _powerBi = powerBI;
         }
 
         public async ValueTask DisposeAsync()
@@ -24,6 +30,21 @@ namespace AstroPanda.Blazor.PowerBI.Services
                 var module = await moduleTask.Value;
                 await module.DisposeAsync();
             }
+
+            if (embedModuleTask.IsValueCreated)
+            {
+                var module = await embedModuleTask.Value;
+                await module.DisposeAsync();
+            }
+        }
+
+
+        public async Task GenerateReport(ElementReference reportContainer, Guid workspaceId, Guid reportId)
+        {
+            Report report = await _powerBi.Reports.GetReportInGroupAsync(workspaceId, reportId);
+
+            var module = await embedModuleTask.Value;
+            await module.InvokeAsync<object>("embedReport");
         }
     }
 }
